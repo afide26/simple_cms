fab28f6b8028e0a5ec3c411423574976a01ac61b
Alan Fidelino <afidedev26@gmail.com>
2015-07-18 17:41:11 +1000

    Add scopes and finished active records

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,10 @@
 class Subject < ActiveRecord::Base
+
+	scope :visible, lambda {where(:visible => true)}
+	scope :invisible, lambda {where(:visible => false)}
+	scope :sorted, lambda {order("subjects.position ASC")}
+	scope :newest_first, lambda {order("subjects.created_at DESC")}
+	# The scope below is an example of a scope with parameters, here the where method
+	# accepted an array instead of just a single data, the syntax shown is SQL
+	scope :search, lambda {|query| where(["name LIKE ?", "%#{query}%"])}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150718045851) do
+ActiveRecord::Schema.define(version: 20150718062710) do
 
   create_table "admin_users", force: :cascade do |t|
     t.string   "first_name",      limit: 25
@@ -54,9 +54,9 @@ ActiveRecord::Schema.define(version: 20150718045851) do
   create_table "subjects", force: :cascade do |t|
     t.string   "name"
     t.integer  "position"
-    t.boolean  "visible"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.boolean  "visible",    default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
